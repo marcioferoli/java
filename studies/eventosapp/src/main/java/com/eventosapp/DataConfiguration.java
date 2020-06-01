4896c20a07df401d57aa1ddddbe188db1259cc7f
Marcio Oliveira <marcioferoli@gmail.com>
2020-05-31 21:12:21 -0300

    Automated commit on Windows

--- studies/eventosapp/src/main/java/com/eventosapp/DataConfiguration.java
+++ studies/eventosapp/src/main/java/com/eventosapp/DataConfiguration.java
@@ -17,8 +17,8 @@ public class DataConfiguration {
 		DriverManagerDataSource dataSource = new DriverManagerDataSource();
 		dataSource.setDriverClassName("com.mysql.jdbc.Driver");
 		dataSource.setUrl("jdbc:mysql://localhost:3306/eventosapp");
-		dataSource.setUsername("");
-		dataSource.setPassword("");
+		dataSource.setUsername("eventosapp");
+		dataSource.setPassword("eventosapp");
 		return dataSource();
 	}
 
