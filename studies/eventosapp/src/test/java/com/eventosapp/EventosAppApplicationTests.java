db84f927f81f8de0bd15f2b777eea720919973cf
Marcio Oliveira <marcioferoli@gmail.com>
2020-06-01 22:41:02 -0300

    Aula 04

--- studies/eventosapp/src/test/java/com/eventosapp/EventosAppApplicationTests.java
+++ studies/eventosapp/src/test/java/com/eventosapp/EventosAppApplicationTests.java
@@ -1,7 +1,14 @@
 package com.eventosapp;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootTest
 class EventosAppApplicationTests {
@@ -9,5 +16,26 @@ class EventosAppApplicationTests {
 	@Test
 	void contextLoads() {
 	}
+
+	@Test
+	void statusCode() throws URISyntaxException {
+		/*
+		long expected = 3;
+		long actual = 4;
+		
+		assertEquals(expected, actual, "The add method should add two numbers");
+		*/
+	
+		RestTemplate restTemplate = new RestTemplate();
+	    URI uri = new URI("http://localhost:8080/");
+	    ResponseEntity<String> result = restTemplate.getForEntity(uri, String.class);
+
+	    // Verify request succeed
+	    assertEquals(200, result.getStatusCodeValue(), "The status code");
+	    // assertEquals(true, result.getBody().contains("employeeList"), "Test");
+	}
+	
+	
+	
 
 }
